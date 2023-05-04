9e486ca521329b5b023d7a47e8ec4da9c615be05
Harshit Srivastava <harshitcsrivastava@gmail.com>
2023-05-04 15:15:00 +0530

    demo

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const app = express()
+const port = 3000
+let bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({
+    extended: true
+}))
+
+
+app.post('/getData', async (req, res) => {
+    let ticket_ID = req.body.Ticket_ID;
+    let assignee_ID = req.body.Assignee_ID;
+    console.log("TicketID", ticket_ID)
+    if (ticket_ID != null && assignee_ID != null) {
+        await fetch(`https://pdi-xoogle.zendesk.com/api/v2/agent_availabilities/${assignee_ID}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Basic ' + btoa('harshit@xoogle.in:Harshit#245')
+            },
+        }).then(response => response.text())
+            .then((data) => {
+                data = JSON.parse(data);
+                let isAgent = data.data.attributes.agent_status.name;
+
+                if (isAgent == "offline") {
+                    fetch(`https://pdi-xoogle.zendesk.com/api/v2/tickets/${ticket_ID}`, {
+                        method: 'PUT',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': 'Basic ' + btoa('harshit@xoogle.in:Harshit#245')
+                        },
+                        body: JSON.stringify({
+                            "ticket":
+                            {
+                                "subject": "API Auth Testing2",
+                                "assignee_id": null,
+                            }
+                        })
+                    }
+                    ).then(response => response.text())
+                        .then((data) => {
+                            console.log("Ticket Updated Successfully")
+                        });
+                }
+            });
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
